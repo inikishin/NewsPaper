23d3067b4274ff069ac5d9c02bf569d1d9496957
Ilya Nikishin <inikishin@gmail.com>
2021-05-07 20:03:23 +0300

    Initial commit

---
+++ NewsPaper/news/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.FloatField(default=0)
+
+    def update_rating(self):
+        qs1 = Post.objects.filter(author=self).aggregate(models.Sum('rating'))
+        qs2 = Comment.objects.filter(user=self.user).aggregate(models.Sum('rating'))
+        qs3 = Comment.objects.filter(post__author=self).aggregate(models.Sum('rating'))
+
+        self.rating = qs1['rating__sum'] * 3 + qs2['rating__sum'] + qs3['rating__sum']
+        self.save()
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255, unique=True)
+
+
+class Post(models.Model):
+    post_types = [
+        ('post', 'статья'),
+        ('news', 'новость')
+    ]
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=10, choices=post_types, default='news')
+    created = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    rating = models.FloatField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    @property
+    def preview(self):
+        return self.content[:124] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    rating = models.FloatField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
